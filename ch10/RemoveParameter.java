cc4e41c8796427b3087e13bc66fa01fffc897b2b
Yuhang Peng <peter.yuhangpeng@gmail.com>
2023-03-19 12:05:15 -0700

    Add RemoveParameter refactoring

---
+++ ch10/RemoveParameter.java
@@ -0,0 +1,12 @@
+package ch10;
+
+/* 
+ * A parameter is no longer used by the method body
+ * 
+ * Remove it
+ * 
+ * - For the polymorphic method case, before remove parameter, should look into other
+ *   implementations of the method. You might choose to add a separate method that can
+ *   be used in those cases, but you need to examine how your callers use the method to
+ *   see whether it is worth doing it
+ */

18a76f15195904fd6145ee696919cfff59832ed2
Yuhang Peng <peter.yuhangpeng@gmail.com>
2023-03-22 20:23:38 -0700

    Add HideMethod refactoring

---
+++ ch10/HideMethod.java
@@ -0,0 +1,11 @@
+package ch10;
+
+/* 
+ * A method is not used by any other class
+ * 
+ * Make the method private
+ * 
+ * - Check regularly for opportunity to make a method more private
+ * - If you make a getting or setting method private and you are using
+ *   direct variable access, you can remove the method
+ */

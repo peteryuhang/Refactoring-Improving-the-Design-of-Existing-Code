c9f4977c7ef5b46c2514b1a7506da1e81d623bec
Yuhang Peng <peter.yuhangpeng@gmail.com>
2023-03-25 10:02:54 -0700

    Add PushDownMethod refactoring

---
+++ ch11/PushDownMethod.java
@@ -0,0 +1,10 @@
+package ch11;
+
+/* 
+ * Behavior on a superclass is relevant only for some of its subclasses
+ * 
+ * Move it to those subclasses
+ * 
+ * - Opposite of PullUpMethod
+ * - Often do this when you use ExtractSubclass
+ */

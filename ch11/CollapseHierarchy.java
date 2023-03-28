8db311cbd80f1130fca6d9013f5715d0523852ca
Yuhang Peng <peter.yuhangpeng@gmail.com>
2023-03-27 20:14:13 -0700

    Add CollapseHierarchy refactoring

---
+++ ch11/CollapseHierarchy.java
@@ -0,0 +1,11 @@
+package ch11;
+
+/* 
+ * A superclass and subclass are not very different
+ * 
+ * Merge them together
+ * 
+ * - Refactoring the hierarchy often involves pushing methods and fields up
+ * and down the hierarchy. After you've done this you can well find you have
+ * a subclass that isn't adding any value, so you need to merge the classes together
+ */
